9ca0d17e212d4ebad1b742fe13ed713a303e5719
Ashley Stewart <a.stewart.au@gmail.com>
2026-01-21 17:32:58 +1000

    Add generic webapp infrastructure

--- config/jupyter/ezbids_launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# ezBIDS launcher for JupyterLab
-# Uses wrapper server to provide instant splash page while container loads
-
-exec python3 /opt/neurodesktop/ezbids_wrapper/ezbids_wrapper.py
