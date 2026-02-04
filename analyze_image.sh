e5949d87a33dd576942622b077fe73632abf9af8
Steffen Bollmann <stebo85@users.noreply.github.com>
2026-02-04 10:56:00 -0800

    fix dive install

--- analyze_image.sh
+++ analyze_image.sh
@@ -1,5 +1,11 @@
 set -e
 
+# test if dive is installed and make sure it is installed
+if ! command -v dive &> /dev/null
+then
+    echo "dive could not be found, installing..."
+    brew install dive
+fi
 
 docker build . -t neurodesktop:latest
 dive neurodesktop --ci > wasted_space.txt
